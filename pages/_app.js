af619df1b4ff75724f652fc8416a5330732da9aa
mahdi <mhf1377@gmail.com>
2021-09-28 12:41:18 +0330

    Add Roboto font

--- pages/_app.js
+++ pages/_app.js
@@ -36,6 +36,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="author" content="deus finance" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="imgs/favicon.ico" />
+        <link href='https://fonts.googleapis.com/css?family=Roboto:500' rel='stylesheet'></link>
       </Head>
 
       <ThemeProvider>
